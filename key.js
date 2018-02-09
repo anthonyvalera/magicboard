c8a168853c2f65c27b034022b26beddcaf024cd5
Anthony Valera <anthony@anthonyvalera.com>
2018-02-09 12:24:00 -0800

    Adds key

---
+++ key.js
@@ -0,0 +1,187 @@
+module.exports = [{
+    id: 1,
+    points: 3,
+    message: 'How would you select an element by an ID?',
+    answer: 'getElementById',
+    include: true
+  },
+  {
+    id: 2,
+    points: 1,
+    message: 'the answer to life',
+    answer: 42
+  },
+  {
+    id: 3,
+    points: 1,
+    message: 'POST /94030nf'
+  },
+  {
+    id: 4,
+    points: 1,
+    message: '/roulette?name=GROUP_NAME'
+  },
+  {
+    id: 5,
+    points: 3,
+    message: 'this^2',
+    answer: '25'
+  },
+  {
+    id: 6,
+    points: 5,
+    message: 'A way to search values in a tree',
+    answer: 'recursion'
+  },
+  {
+    id: 7,
+    points: 3,
+    message: '10!',
+    answer: 3628800
+  },
+  {
+    id: 8,
+    points: 1,
+    message: 'Properly update a component\'s state',
+    answer: 'setState',
+    include: true
+  },
+  {
+    id: 9,
+    points: 1,
+    message: 'Character to define a generator function',
+    answer: '*',
+    include: true
+  },
+  {
+    id: 10,
+    points: 1,
+    message: 'Internal Server Error',
+    answer: 500
+  },
+  {
+    id: 11,
+    points: 3,
+    message: 'Animation functions in CSS',
+    answer: '@keyframes'
+  },
+  {
+    id: 12,
+    points: 3,
+    message: '1100',
+    answer: 12
+  },
+  {
+    id: 13,
+    points: 3,
+    message: 'Newest primitive data type',
+    answer: 'symbol'
+  },
+  {
+    id: 14,
+    points: 1,
+    message: 'Hello',
+    answer: 'World'
+  },
+  {
+    id: 15,
+    points: 1,
+    message: '(!![] + + !![] * 2 ) * 5',
+    answer: 15
+  },
+  {
+    id: 16,
+    points: 1,
+    message: '[NaN + \'a\'].reduce((a,b) => a + b, \'ba\')',
+    answer: 'banana'
+  },
+  {
+    id: 17,
+    points: 5,
+    message: '/e.+/g => GET /14',
+    answer: 'ello'
+  },
+  {
+    id: 18,
+    points: 3,
+    message: 'this',
+    answer: '{}'
+  },
+  {
+    id: 19,
+    points: 3,
+    message: 'after every then(), we must',
+    answer: 'catch',
+    include: true
+  },
+  {
+    id: 20,
+    points: 3,
+    message: 'Elements within the Document Object Model are called...',
+    answer: 'nodes',
+    include: true
+  },
+  {
+    id: 21,
+    points: 1,
+    message: 'What do we use for version control?',
+    answer: 'git'
+  },
+  {
+    id: 22,
+    points: 1,
+    message: 'What is Justin\'s favorite color?',
+    answer: 'pink'
+  },
+  {
+    id: 23,
+    points: 1,
+    message: 'What is Michael B\'s favorite climbing gear?',
+    answer: 'grigri'
+  },
+  {
+    id: 24,
+    points: 1,
+    message: 'When day is Anthony\'s birthday? Hint: its this month!',
+    answer: '24',
+    include: true
+  },
+  {
+    id: 25,
+    points: 1,
+    message: 'What is Jeff\'s favorite football team?',
+    answer: 'colts'
+  },
+  {
+    id: 26,
+    points: 1,
+    message: 'How many students were in Origin\'s first class?',
+    answer: 6
+  },
+  {
+    id: 27,
+    points: 1,
+    message: 'What is Christian\'s favorite surf spot?',
+    answer: 'pb point',
+    include: true
+  },
+  {
+    id: 28,
+    points: 1,
+    message: 'Where is Michael R.\'s hometown?',
+    answer: 'boston'
+  },
+  {
+    id: 29,
+    points: 1,
+    message: 'What is Ryan\'s favorite video game?',
+    answer: 'verdun'
+  },
+  {
+    id: 30,
+    points: 1,
+    message: 'What universe does Origin use for naming things internally?',
+    answer: 'star wars',
+    include: true
+  }
+];
